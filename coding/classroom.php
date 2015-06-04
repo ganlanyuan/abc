c32e1297fc85fcfe26709abc693996478e9aff8f
designer <ganlanyuan@gmail.com>
2015-06-04 14:14:53 -0400

    discussion css done

--- coding/classroom.php
+++ coding/classroom.php
@@ -21,9 +21,9 @@
             <span class="abc-arrow-up"></span>
           </div>
           <h4 class="heading4 minor"><span class="abc-speaker icons-fill icons-sm"></span>economics 302: global trends</h4>
-          <p class="minor">Dear Students:</p>
-          <p class="minor">The midterm will be next Wednesday. Remember, it counts for 30% of your grade. If you have any questions, please visit my office hours at 2PM on Tuesday.</p>
-          <p class="minor">Good luck!</p>
+          <p>Dear Students:</p>
+          <p>The midterm will be next Wednesday. Remember, it counts for 30% of your grade. If you have any questions, please visit my office hours at 2PM on Tuesday.</p>
+          <p>Good luck!</p>
           <div class="file-name"><a href="">GarlicFries_927x365.jpg</a></div>
         </div>
         <div class="panel-body inner">
@@ -36,11 +36,11 @@
             </div>
           </header>
           <h4 class="heading4 minor"><span class="abc-speaker icons-fill icons-sm"></span>welcome to week 1</h4>
-          <p class="minor">Welcome Analyzing Global Trends students!</p>
-          <p class="minor">You will be starting a journey around the world in pursuit of global trends that are fundamentally reshaping the economic, political, and social landscape.</p>
-          <p class="minor">On monday, beginning at 12:01 EST, you will be able to access the videos for the first week. Watch the videos, complete the opinion surveys, take the quizzes, and interact with one another on our social media sites and discussion forum. Focus your attention on the big changes taking place in the world. Try to understand their causes, and their consequences.</p>
-          <p class="minor">And please remember take our Global Literacy Test and the first week's opinion survey.</p>
-          <p class="minor">Enjoy!</p>
+          <p>Welcome Analyzing Global Trends students!</p>
+          <p>You will be starting a journey around the world in pursuit of global trends that are fundamentally reshaping the economic, political, and social landscape.</p>
+          <p>On monday, beginning at 12:01 EST, you will be able to access the videos for the first week. Watch the videos, complete the opinion surveys, take the quizzes, and interact with one another on our social media sites and discussion forum. Focus your attention on the big changes taking place in the world. Try to understand their causes, and their consequences.</p>
+          <p>And please remember take our Global Literacy Test and the first week's opinion survey.</p>
+          <p>Enjoy!</p>
 
         </div>
       </div>
--- coding/discussion.php
+++ coding/discussion.php
@@ -10,15 +10,70 @@
     <div class="main">
       <div class="alert-danger">Discussion: Economics 302: Global Trends  /  Week 1 : Globalization</div>
       <div class="panel panel-pure">
-        Dear Students:
-        Please do the following in this discussion board
+        <button class="button button-normal floated">Edit</button>
+        <p>Dear Students: <br /><strong>Please do the following in this discussion board</strong></p>
+        <ol class="circle-number">
+          <li class="heading4">Find an article relevant to globalization</li>
+          <li class="heading4">Post at least 100 words on your analysis or the article</li>
+          <li class="heading4">Reply to at least 3 other student's post</li>
+        </ol>
+        <p>I look forward to a thrilling discussion!</p>
       </div>
-      <div class="panel">
+      <div class="action-line">
+        <button class="button button-alert">new thread</button>
+        <button class="button button-normal">1</button>
+        <button class="button button-normal current">2</button>
+        <button class="button button-normal">3</button>
+      </div>
+      <div class="panel panel-table">
         <div class="panel-head">
           <span class="abc- icons-lg"></span>
           <h3 class="heading3">week 1: globalization: thread list</h3>
         </div>
         <div class="panel-body">
+          <table class="table-striped table-responsive table-thread">
+            <thead>
+              <th>Thread / thread Starter</th>
+              <th>latest post</th>
+              <th>replies</th>
+              <th>views</th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <h4 class="heading4"><a href="#">Globalization vs Innovation</a></h4>
+                  <p>Jane Doe, Fri Jan 16 13:29:53 PST 2015</p>
+                </td>
+                <td><p>Fri Jan 16 13:33:43 PST 2015, by Edward Jones</p></td>
+                <td><p>4</p></td>
+                <td><p>7</p></td>
+                </tr>
+              <tr>
+                <td>
+                  <h4 class="heading4"><a href="#">largest consumer markets - india & china will it take 20 years?</a></h4>
+                  <p>Jane Doe, Fri Jan 16 13:29:53 PST 2015</p></td>
+                <td><p>Fri Jan 16 13:33:43 PST 2015, by Edward Jones</p></td>
+                <td><p>4</p></td>
+                <td><p>7</p></td>
+                </tr>
+              <tr>
+                <td>
+                  <h4 class="heading4"><a href="#">Globalization vs Innovation</a></h4>
+                  <p>Jane Doe, Fri Jan 16 13:29:53 PST 2015</p></td>
+                <td><p>Fri Jan 16 13:33:43 PST 2015, by Edward Jones</p></td>
+                <td><p>4</p></td>
+                <td><p>7</p></td>
+              </tr>
+              <tr>
+                <td>
+                  <h4 class="heading4"><a href="#">Globalization vs Innovation</a></h4>
+                  <p>Jane Doe, Fri Jan 16 13:29:53 PST 2015</p></td>
+                <td><p>Fri Jan 16 13:33:43 PST 2015, by Edward Jones</p></td>
+                <td><p>4</p></td>
+                <td><p>7</p></td>
+                </tr>
+              </tbody>
+            </table>
         </div>
       </div>
     </div>
