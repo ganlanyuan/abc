634b9f1af5ba2e0e67000261fe4e40c6d5d9ee96
william-D <ganlanyuan@gmail.com>
2015-06-26 15:45:25 -0400

    rm code in script.js

--- coding/dev/js/script.js
+++ coding/dev/js/script.js
@@ -2,23 +2,23 @@
 // @codekit-prepend "../../bower_components/responsive-img.js/responsive-img.js"
 // @codekit-prepend "../../bower_components/rocket/src/js/kit.js"
 
-ready(function () {
-  k('.button-toggle').click(function() {
-    k(this).parents('.panel').toggleClass('panel-close');
-    k(this).find('span').toggleClass('abc-angle-up').toggleClass('abc-angle-down');
-  });
+// ready(function () {
+//   k('.button-toggle').click(function() {
+//     k(this).parents('.panel').toggleClass('panel-close');
+//     k(this).find('span').toggleClass('abc-angle-up').toggleClass('abc-angle-down');
+//   });
 
-  // video list
-  k('.video-list .toggle').click(function() {
-    k(this).parents('li').toggleClass('show');
-  });
+//   // video list
+//   k('.video-list .toggle').click(function() {
+//     k(this).parents('li').toggleClass('show');
+//   });
 
-  // popover
-  k('.popover > .button').click(function(event) {
-    k(this).siblings('.popover-content').toggleClass('show');
-    event.preventDefault ? event.preventDefault() : event.returnValue = false;
-  });
-  k('.popover .cancel').click(function() {
-    k(this).parents('.popover-content').removeClass('show');
-  });
-});
+//   // popover
+//   k('.popover > .button').click(function(event) {
+//     k(this).siblings('.popover-content').toggleClass('show');
+//     event.preventDefault ? event.preventDefault() : event.returnValue = false;
+//   });
+//   k('.popover .cancel').click(function() {
+//     k(this).parents('.popover-content').removeClass('show');
+//   });
+// });
